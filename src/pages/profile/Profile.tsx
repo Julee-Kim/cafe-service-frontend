453c83d5110ddd3e669d792f1b55a11adae4db8a
julee kim <summerbreeze0323@gmail.com>
2021-04-19 13:29:26 +0900

    내 정보 - 주소 입력 부분 addressForm 컴포넌트로 변경

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -4,11 +4,18 @@ import { useForm } from 'react-hook-form';
 import { InputWrap } from '../../components/InputWrap';
 import { ModalChangePassword } from './components/ModalChangePassword';
 import { Button } from '../../components/Button';
+import { AddressForm } from '../../components/AddressForm';
 
 declare global {
   interface Window {
     daum: any;
   }
+}
+
+interface IAddressData {
+  zonecode: string;
+  address: string;
+  addressDetail: string;
 }
 
 interface IProfile {
@@ -23,9 +30,11 @@ interface IProfile {
 
 export const Profile = () => {
   const [showModal, setShoModal] = useState(false);
-  const [zonecode, setZonecode] = useState('');
-  const [address, setAddress] = useState('');
-  const [addressDetail, setAddressDetail] = useState('')
+  const [addressData, setaddressData] = useState<IAddressData>({
+    zonecode: '',
+    address: '',
+    addressDetail: '',
+  });
   const {
     register,
     getValues,
@@ -34,31 +43,15 @@ export const Profile = () => {
     formState,
   } = useForm<IProfile>({ mode: 'onChange' });
 
-  const execPostCode = () => {
-    new window.daum.Postcode({
-      // 팝업에서 검색결과 항목을 클릭했을때 실행
-      oncomplete(data: any) {
-        // 우편번호와 주소 정보를 해당 필드에 넣는다.
-        setZonecode(data.zonecode);
-
-        // 주소 타입(지번: J, 도로명: R) 체크
-        switch(data.userSelectedType) {
-          case 'R':
-            setAddress(data.roadAddress);
-            break;
-
-          case 'J':
-            setAddress(data.jibunAddress);
-            break;
-        }
-      }
-    }).open();
-  };
+  const setAddData = (data: IAddressData) => {
+    console.log('-- setAddData')
+    console.log(data)
+  }
 
   const onSubmit = () => {
     const { name, birth, gender, email } = getValues();
     const data = {
-      name, birth, gender, email, zonecode, address, addressDetail
+      name, birth, gender, email,
     };
     console.log(data)
   }
@@ -99,40 +92,11 @@ export const Profile = () => {
             />
             <div className="password">
               <label htmlFor="currentPassword">비밀번호</label>
-              <button type="button" className="btn able btn_change_password btn_mint" onClick={() => setShoModal(true)}>비밀번호 변경</button>
+              <button type="button" className="btn able btn_change_password btn_mint m-0-import" onClick={() => setShoModal(true)}>비밀번호 변경</button>
             </div>
           </div>
           <div className="address_wrap input_wrap">
-            <label htmlFor="zonecode">주소</label>
-            <div className="zonecode_wrap">
-              <input
-                name="zonecode"
-                id="zonecode"
-                className="zonecode"
-                placeholder="우편번호"
-                value={zonecode}
-                readOnly
-              />
-              <button type="button" className="btn btn_search_addr" onClick={execPostCode}>주소 검색</button>
-            </div>
-            <div className="grid lg:grid-cols-2 lg:gap-2">
-              <input
-                name="address"
-                id="address"
-                className="address"
-                placeholder="도로명 주소"
-                value={address}
-                readOnly
-              />
-              <input
-                name="addressDetail"
-                id="addressDetail"
-                className="address_detail"
-                placeholder="상세주소 입력"
-                value={addressDetail}
-                onChange={(e) => setAddressDetail(e.target.value)}
-              />
-            </div>  
+            <AddressForm setAddData={setAddData}/>
           </div>
           <Button
             canClick={formState.isValid}
@@ -142,11 +106,6 @@ export const Profile = () => {
         </form>
       </div>
 
-      {/* Daum postcode */}
-      <DaumPostcode
-        onComplete={execPostCode}
-        style={{height: 0}}
-      />
       <ModalChangePassword show={showModal} setShoModal={setShoModal}/>
     </div>
   )
